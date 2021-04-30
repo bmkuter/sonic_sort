a651956983e148ffd8decb84853d451334933799
Gomez <santi09@signals23.bu.edu>
2021-04-30 15:56:53 -0400

    works with 4 threads & 2 blocks

--- sonic_sort.cu
+++ sonic_sort.cu
@@ -23,11 +23,11 @@ inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
 }
 //16777216
 //#define BIG_SERIAL_INT              4096
-#define NUM_THREADS_PER_SORT        4
+#define NUM_THREADS_PER_SORT        2
 
-#define SM_ARR_LEN                  256
-#define THREADS_PER_BLOCK_MERGE     64
-#define BLOCKS                      4 /* Has to be at least NUM_THREADS_PER_SORT */
+#define SM_ARR_LEN                  64
+#define THREADS_PER_BLOCK_MERGE     32
+#define BLOCKS                      2 /* Has to be at least NUM_THREADS_PER_SORT */
 
 #define PRINT_TIME                  1
 
@@ -213,26 +213,31 @@ int main(int argc, char **argv){
 #endif
       /* https://forums.developer.nvidia.com/t/size-limitation-for-1d-arrays-in-cuda/31066 */
 
+  unsigned int num_elem_per_thread = arrLen/NUM_THREADS_PER_SORT;
+  
+  sonic_sort<<<1, NUM_THREADS_PER_SORT>>>(d_arrayA, d_arrayB, arrLen, num_elem_per_thread); //4
 
-  sonic_sort<<<1, NUM_THREADS_PER_SORT>>>(d_arrayA, d_arrayB, arrLen, arrLen/NUM_THREADS_PER_SORT); //4
-
-  int i,j;
+  int i,j, k;
 
   unsigned int half_array_length = arrLen >> 1; //32 / 2 = 16
 
-  for ( i = log2((float)NUM_THREADS_PER_SORT); i > 0; i >>= 1)
-  //for ( i = 1; i < NUM_THREADS_PER_SORT ; i <<= 1)
+  unsigned int num_inner_loops = NUM_THREADS_PER_SORT >> 1;
+  
+  //for ( i = log2((float)NUM_THREADS_PER_SORT); i > 0; i >>= 1)
+  for ( i = 1; i < NUM_THREADS_PER_SORT ; i <<= 1)
   {
-      for ( j = 0; j <= i; j += 2)
-      //for ( j = 0, k = 0; k < num_inner_loops; j = j + (2*i),k++)
+      
+      
+      //for ( j = 0; j <= i; j += 2)
+      for ( j = 0, k = 0; k < num_inner_loops; j = j + (2*i),k++)
       {
-          mega_merge<<<dimGrid, dimBlock >>>(d_arrayA+(j*(half_array_length/i)), d_arrayA+((j+1)*(half_array_length/i)), d_arrayC+(j*(half_array_length/i)),half_array_length/i);
-          //mega_merge<<<dimGrid, (arrLen/i)>>>(d_arrayA+j, d_arrayA+(j+i), d_arrayC+j,half_array_length/i);
-          cudaDeviceSynchronize();
+          //mega_merge<<<dimGrid, dimBlock >>>(d_arrayA+(j*(half_array_length/i)), d_arrayA+((j+1)*(half_array_length/i)), d_arrayC+(j*(half_array_length/i)),half_array_length/i);
+          mega_merge<<<dimGrid, dimBlock>>>(d_arrayA+j, d_arrayA+(j+i*num_elem_per_thread), d_arrayC+j,num_elem_per_thread);
+          cudaDeviceSynchronize(); 
 
       }
-      //num_inner_loops >>= 1;
-
+      num_inner_loops >>= 1;
+      num_elem_per_thread <<= 1;
       temp = d_arrayA;
       d_arrayA = d_arrayC;
       d_arrayC = temp;
@@ -262,7 +267,7 @@ int main(int argc, char **argv){
 //HOST SOR. Use copied SOR function here.
 
   printf("Cuda Array (arrayA): \n");
-  //printArray(h_arrayA,SM_ARR_LEN);
+  printArray(h_arrayA,SM_ARR_LEN);
   printf ("\n");
 
 /* Serial Land */
@@ -282,7 +287,7 @@ int main(int argc, char **argv){
   printf("CPU Time: %.6f (msec)\n",1000*time_stamp);
 
   printf("Cuda Array (arraySerial): \n");
-  //printArray(h_arraySerial,SM_ARR_LEN);
+  printArray(h_arraySerial,SM_ARR_LEN);
   printf ("\n");
 
   if (compare_lists(h_arraySerial,h_arrayA,SM_ARR_LEN) != 0) printf("List comparision failed!\n");
